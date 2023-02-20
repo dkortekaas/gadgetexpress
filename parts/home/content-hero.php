70c28c93acd0298f32e8e70217806fb2029baad1
Dennis Kortekaas <d.kortekaas@icloud.com>
2023-02-20 21:19:00 +0100

    Header image

--- parts/home/content-hero.php
+++ parts/home/content-hero.php
@@ -9,7 +9,7 @@
 
 <style>
 @media only screen and (min-width: 1168px) {
-.slider-wrapper {height:550px !important;}
+.slider-wrapper {height:500px !important;}
 }
 </style>
 
@@ -17,10 +17,10 @@
         <div class="slider-wrapper">
             <ul>
                 <li>
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/header.jpg'; ?>" alt="Gadget Express - Logo" title="<?php _e('Buy the coolest gadgets at Gadget Express!', 'gadget'); ?>" width="1920" height="960" class="slide">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/gadgetexpress-header.jpg'; ?>" alt="Gadget Express - Logo" title="<?php _e('Buy the coolest gadgets at Gadget Express!', 'gadget'); ?>" width="1400" height="500" class="slide">
                     <div class="slider-caption">
                         <?php _e('Buy the coolest gadgets at Gadget Express!', 'gadget'); ?><br/>
-                        <a class="btn btn-primary slider-btn" href="/producten" title="<?php _e('Shop Products', 'gadget'); ?>"><?php _e('Shop Products', 'gadget'); ?></a>
+                        <a class="btn btn-primary slider-btn" href="/gadgets" title="<?php _e('Shop Gadgets', 'gadget'); ?>"><?php _e('Shop Gadgets', 'gadget'); ?></a>
                     </div>
                 </li>
             </ul>
